679eb8aac345f61409ca4af0fd7efd899a8f508e
Maybe a Replicant <dan990ov@gmail.com>
2024-06-25 10:01:49 +0200

    Completion of exercises 01 through 07, inclusive.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+  let string = "";
+  if (times < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < times; i++) {
+      string += word;
+    }
+    return string;
+  }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fDegrees) {
+  let t = (fDegrees - 32) * (5 / 9);
+  return Math.round(t * 1e1) / 1e1;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cDegrees) {
+  let t = cDegrees * (9 / 5) + 32;
+  return Math.round(t * 1e1) / 1e1;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
