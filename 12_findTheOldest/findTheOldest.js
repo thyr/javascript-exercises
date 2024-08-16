84fc5e8c797b81686e9271e7c5cd66cd4022f390
Maybe a Replicant <dan990ov@gmail.com>
2024-08-16 12:12:24 +0200

    Completion of exercises 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const getAge = function (person) {
+  person.yearOfDeath ??= new Date().getFullYear();
 
+  return person.yearOfDeath - person.yearOfBirth;
+};
+
+const findTheOldest = function (peopleArr) {
+  const peopleOldestToYoungest = peopleArr.toSorted(
+    (person, nextPerson) => getAge(nextPerson) - getAge(person)
+  );
+  const oldestPerson = peopleOldestToYoungest[0];
+  return oldestPerson;
 };
 
 // Do not edit below this line
