01efc9d57d6f7f127de2f4cafb21b1d19f174f54
Maybe a Replicant <dan990ov@gmail.com>
2024-08-16 10:30:12 +0200

    Completion of exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let alphanumerical = "abcdefghijklmnopqrstuvwxyz1234567890";
 
+  let cleanString = string
+    .toLowerCase()
+    .split("")
+    .filter((character) => alphanumerical.includes(character))
+    .join("");
+
+  let reversedString = cleanString.split("").reverse().join("");
+
+  return cleanString == reversedString;
 };
 
 // Do not edit below this line
