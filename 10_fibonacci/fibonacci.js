3aa60592dba18a947fe2d1f9015dd43ded3cd041
Maybe a Replicant <dan990ov@gmail.com>
2024-08-16 11:12:06 +0200

    Completion of exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (fibArg) {
+  let fibCount;
+  if (typeof fibArg !== "number") {
+    fibCount = parseInt(fibArg);
+  } else {
+    fibCount = fibArg;
+  }
 
+  if (fibCount < 0) return "OOPS";
+  if (fibCount == 0) return 0;
+
+  let fibN1 = 1, fibN2 = 0;
+
+  for (let i = 2; i <= fibCount; i++) {
+    let fibCurrent = fibN1 + fibN2;
+    fibN2 = fibN1;
+    fibN1 = fibCurrent;
+  }
+
+  return fibN1;
 };
 
 // Do not edit below this line
